1c28ad2208455cc7d6ea3fc6b95cc962fdcc6815
AndreVelde <andre.vandevelde@ns.nl>
2025-01-29 09:49:55 +0100

    Bugfix fix win condition for horizontal wins

--- app/tic-tac-toe.ts
+++ app/tic-tac-toe.ts
@@ -14,7 +14,7 @@ export class TicTacToe {
         // Horizontal wins
         [MoveOptions.TOP_LEFT, MoveOptions.TOP_MIDDLE, MoveOptions.TOP_RIGHT],
         [MoveOptions.MIDDLE_LEFT, MoveOptions.MIDDLE, MoveOptions.MIDDLE_RIGHT],
-        [MoveOptions.BOTTOM_RIGHT, MoveOptions.BOTTOM_MIDDLE, MoveOptions.BOTTOM_RIGHT],
+        [MoveOptions.BOTTOM_LEFT, MoveOptions.BOTTOM_MIDDLE, MoveOptions.BOTTOM_RIGHT],
 
         // Diagonal wins
         [MoveOptions.TOP_LEFT, MoveOptions.MIDDLE, MoveOptions.BOTTOM_RIGHT],
